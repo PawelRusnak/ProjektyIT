3697e42af94d4638763fd948b0359277a23bcc1c
Paweł Rusnak <Pawel.Rusnak@cshark.com>
2020-05-13 17:48:07 +0200

    Updates

---
+++ precache-manifest.2bb00ed3a68910464c8deaef7ec554a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23e1eb3bfb5d71e9226110d8dfe46ef",
+    "url": "/ProjektyIT/index.html"
+  },
+  {
+    "revision": "45b8b22112a17c28bc14",
+    "url": "/ProjektyIT/static/css/main.8a0c3ef3.chunk.css"
+  },
+  {
+    "revision": "9f815b85d45365d85edb",
+    "url": "/ProjektyIT/static/js/2.df76ae41.chunk.js"
+  },
+  {
+    "revision": "780dcd34534fbf6955ad8d91034e2a51",
+    "url": "/ProjektyIT/static/js/2.df76ae41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b8b22112a17c28bc14",
+    "url": "/ProjektyIT/static/js/main.ccaa26ee.chunk.js"
+  },
+  {
+    "revision": "1a927b553a75741a721b",
+    "url": "/ProjektyIT/static/js/runtime-main.8ccb27f9.js"
+  }
+]);
